53c4bc2b360d6585cc02d3e98fec0481471c5668
Armard Bellamy <armardbellamy@gmail.com>
2018-09-19 18:25:36 -0400

    Setup boilerplate for project

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'BMI Calculator',
+    home: new BmiHome(),
+  ));
+}
---
+++ lib/ui/BmiHome.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BmiHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new BMIState();
+  }
+
+
+}
+
+class BMIState extends State<BmiHome> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+    );
+  }
+
+}
